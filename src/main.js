30bb34919059f1b882c804628e65597a99e8485b
Gustav Brodin <brodin.gustav@gmail.com>
2024-05-12 12:32:10 +0200

    Commit - Lagt till funktino för registrering samt lagrat URLs för fetch mot databas

---
+++ src/main.js
@@ -0,0 +1,18 @@
+//Definierar URL:S
+const apiUrl = "http://localhost:3000/api";
+const url_Register = `${apiUrl}/register`;
+const url_LogIn = `${apiUrl}/login`;
+const url_Protected = `${apiUrl}/protected`;
+
+//Funktion för att registrera användare
+document.getElementById("registerForm").addEventListener("submit", async function(event) {
+
+    event.preventDefault();
+    
+    //Hämtar in input från ID:fält
+    const username = document.getElementById("registerUsername").value;
+    const password = document.getElementById("registerPassword").value;
+
+    await createUser(username, password);
+});
+
